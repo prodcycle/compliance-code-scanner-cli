c6ae5f659b05978b7c5e89d01745b11670cf782c
Gian Colombi <gian@prodcycle.com>
2026-04-08 17:34:19 -0400

    chore: obscure python package source code using cython

---
+++ python/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import os
+import glob
+
+# Collect all Python files in src/compliance_code_scanner/
+extensions = []
+for root, _, files in os.walk('src/compliance_code_scanner'):
+    for file in files:
+        if file.endswith('.py') and file != '__init__.py':
+            filepath = os.path.join(root, file)
+            module_name = filepath.replace('src/', '').replace('/', '.').replace('.py', '')
+            extensions.append(Extension(module_name, [filepath]))
+
+# Keep __init__.py purely as python file for package resolution
+setup(
+    ext_modules=cythonize(extensions, compiler_directives={'language_level': "3"})
+)
